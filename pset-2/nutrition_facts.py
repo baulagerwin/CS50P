41ad9ff2d61d2b069da8862003cfa17b1ebfa69b
Gerwin Baula <baulagerwin@gmail.com>
2022-09-02 22:45:38 +0800

    bug fixed in nutrition facts

--- pset-2/nutrition_facts.py
+++ pset-2/nutrition_facts.py
@@ -1,34 +1,32 @@
 def main():
     fruits = [
-        { "item": "apple", "calories": "130" },
-        { "item": "avocado", "calories": "50" },
-        { "item": "banana", "calories": "110" },
-        { "item": "cantaloupe", "calories": "50" },
-        { "item": "grapefruit", "calories": "60" },
-        { "item": "grapes", "calories": "90" },
-        { "item": "honeydrew melon", "calories": "50" },
-        { "item": "kiwifruit", "calories": "90" },
-        { "item": "lemon", "calories": "15" },
-        { "item": "lime", "calories": "20" },
-        { "item": "nectarine", "calories": "60" },
-        { "item": "orange", "calories": "80" },
-        { "item": "peach", "calories": "60" },
-        { "item": "pear", "calories": "100" },
-        { "item": "pineapple", "calories": "50" },
-        { "item": "plums", "calories": "70" },
-        { "item": "strawberries", "calories": "50" },
-        { "item": "sweet cherries", "calories": "100" },
-        { "item": "tangerine", "calories": "50" },
-        { "item": "watermelon", "calories": "80" }
+        { "apple": "130" },
+        { "avocado": "50" },
+        { "banana": "110" },
+        { "cantaloupe": "50" },
+        { "grapefruit": "60" },
+        { "grapes": "90" },
+        { "honeydrew melon": "50" },
+        { "kiwifruit": "90" },
+        { "lemon": "15" },
+        { "lime": "20" },
+        { "nectarine": "60" },
+        { "orange": "80" },
+        { "peach": "60" },
+        { "pear": "100" },
+        { "pineapple": "50" },
+        { "plums": "70" },
+        { "strawberries": "50" },
+        { "sweet cherries": "100" },
+        { "tangerine": "50" },
+        { "watermelon": "80" }
     ]
     
     item = input("Item: ").strip().lower()
     
     for fruit in fruits:
-        if "item" in fruit and fruit["item"] == item:
-            calories = fruit["calories"]
-        else:
-            calories = ""
+        if item in fruit:
+            print(f"Calories: {fruit[item]}")
+            break
         
-    print(f"Calories: {calories}")
 main()
